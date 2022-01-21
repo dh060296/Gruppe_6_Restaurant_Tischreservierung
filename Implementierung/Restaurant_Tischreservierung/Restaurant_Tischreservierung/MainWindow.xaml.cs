dacd0fe077750e64e272d80ff809bb0098670494
Daniel Hofmann <d.h0602@yahoo.de>
2022-01-21 16:32:25 +0100

    mini commit

--- Implementierung/Restaurant_Tischreservierung/Restaurant_Tischreservierung/MainWindow.xaml.cs
+++ Implementierung/Restaurant_Tischreservierung/Restaurant_Tischreservierung/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +22,39 @@ namespace Restaurant_Tischreservierung
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ICollectionView CollectionView;
+        private tischreservierungEntities ctx = new tischreservierungEntities();
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Btn_Reservierung_Click(object sender, RoutedEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            ctx.Reservierung.Load();
+            CollectionView = CollectionViewSource.GetDefaultView(ctx.Reservierung.Local);
+            ParentGrid.DataContext = CollectionView;
+        }
 
+        private void Btn_Reservierung_Click(object sender, RoutedEventArgs e)
+        {  
+            //string name = Name.Text;
+            //int telefonnummer = Convert.ToInt32(Telefonnummer.Text);
+            //int tischnummer = Convert.ToInt32(Tischnummer.Text);
+            //DateTime reservierungsDatum = (DateTime)Reservierungsdatum.SelectedDate;
+
+            //Kunde NeuerKunde = new Kunde();
+            //NeuerKunde.Name = name;
+            //NeuerKunde.Telefonnummer = telefonnummer;
+
+            //Reservierung rsv = new Reservierung();
+            //rsv.Reservierungsdatum = reservierungsDatum;
+            //rsv.Datum = DateTime.Now;
+
+            //ctx.Kunde.Add(NeuerKunde);
+            //ctx.Reservierung.Add(rsv);
+
+            ctx.SaveChanges();
         }
     }
 }
